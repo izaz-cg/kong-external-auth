10f0f0cc4dbd00c8629767a1365f70a3feea6da9
izaz-cg <88095583+izaz-cg@users.noreply.github.com>
2022-04-27 15:04:39 +0530

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -1,15 +1,15 @@
 return {
   no_consumer = true,
   fields = {
-    url = { required = true, type = "url" },
-    path = { default = "/verify/user", type = "string" },
+    url = { required = true, type = "url", default = "http://user.dev.svc.cluster.local:8000" },
+    path = { default = "/user/public/verify/user", type = "string" },
     connect_timeout = { default = 10000, type = "number" },
     send_timeout = { default = 60000, type = "number" },
     read_timeout = { default = 60000, type = "number" },
     message_401 = { default = "Session expired, please login again", type = "string" },
     message_403 = { default = "You don't have enough permissions to access", type = "string" },
     message_404 = { default = "Not Found", type = "string" },
-    injection_header = { default = "cg-user", type = "string" },
+    injection_header = { default = "X-CG-User", type = "string" },
     company_injection_header = { default = "cg-company", type = "string" },
     token_header = { default = "authenticationtoken", type = "string" }
   }
