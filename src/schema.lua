f50cfe4447fb293bff31bf3b8067ddc58af4a052
aditya gupta <adityagupta93999@gmail.com>
2022-04-27 13:48:02 +0530

    added company_injection_header

--- src/schema.lua
+++ src/schema.lua
@@ -6,10 +6,11 @@ return {
     connect_timeout = { default = 10000, type = "number" },
     send_timeout = { default = 60000, type = "number" },
     read_timeout = { default = 60000, type = "number" },
-    message_401 = { default = "Unauthorized", type = "string" },
+    message_401 = { default = "Session expired, please login again", type = "string" },
     message_403 = { default = "You don't have enough permissions to access", type = "string" },
     message_404 = { default = "Not Found", type = "string" },
     injection_header = { default = "cg-user", type = "string" },
+    company_injection_header = { default = "cg-company", type = "string" },
     token_header = { default = "authenticationtoken", type = "string" }
   }
 }
