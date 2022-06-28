ad837764176cf6878b7d320a6324713b87bb1ec1
izaz-cg <88095583+izaz-cg@users.noreply.github.com>
2022-06-28 14:37:05 +0530

    sync

--- src/schema.lua
+++ src/schema.lua
@@ -1,15 +1,15 @@
-    return {
-      no_consumer = true,
-      fields = {
-        url = { required = true, type = "url", default = "http://payment-user-uat.service.uat.svc.cluster.local:8000" },
-        path = { default = "/payment-user/verify_token", type = "string" },
-        connect_timeout = { default = 10000, type = "number" },
-        send_timeout = { default = 60000, type = "number" },
-        read_timeout = { default = 60000, type = "number" },
-        message_401 = { default = "Session expired, please login again", type = "string" },
-        message_403 = { default = "You don't have enough permissions to access", type = "string" },
-        message_404 = { default = "Not Found", type = "string" },
-        injection_header = { default = "X-CG-User", type = "string" },
-        token_header = { default = "Authorization", type = "string" }
-      }
+return {
+    no_consumer = true,
+    fields = {
+      url = { required = true, type = "url", default = "http://payment-user.uat.svc.cluster.local:8000" },
+      path = { default = "/payment-user/verify_token", type = "string" },
+      connect_timeout = { default = 10000, type = "number" },
+      send_timeout = { default = 60000, type = "number" },
+      read_timeout = { default = 60000, type = "number" },
+      message_401 = { default = "Session expired, please login again", type = "string" },
+      message_403 = { default = "You don't have enough permissions to access", type = "string" },
+      message_404 = { default = "Not Found", type = "string" },
+      injection_header = { default = "X-CG-User", type = "string" },
+      token_header = { default = "Authorization", type = "string" }
     }
+  }
