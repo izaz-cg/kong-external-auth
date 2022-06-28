5450a67d4a885a3148e440f8631fdb9aa0444109
izaz-cg <88095583+izaz-cg@users.noreply.github.com>
2022-06-28 14:35:07 +0530

    sync

--- src/handler.lua
+++ src/handler.lua
@@ -1,88 +1,88 @@
-        local BasePlugin = require "kong.plugins.base_plugin"
-        local http = require "resty.http"
-        local json = require "cjson"
+local BasePlugin = require "kong.plugins.base_plugin"
+local http = require "resty.http"
+local json = require "cjson"
 
-        local kong = kong
+local kong = kong
 
-        local ExternalAuthHandler = BasePlugin:extend()
+local ExternalAuthHandler = BasePlugin:extend()
 
-        function ExternalAuthHandler:new()
-          ExternalAuthHandler.super.new(self, "external-auth")
+function ExternalAuthHandler:new()
+  ExternalAuthHandler.super.new(self, "external-auth")
+end
+
+function get_company_id()
+  local company_id = kong.request.get_query_arg("company_id")
+  if not company_id then
+    local body, err, mimetype = kong.request.get_body()
+    if body then
+      if mimetype == "application/json" then
+        company_id = body.company_id
+      else
+        if mimetype == "multipart/form-data" then
+          company_id = body.company_id
         end
+      end
+    end
+  end
+  return company_id
+end
 
-        function get_company_id()
-          local company_id = kong.request.get_query_arg("company_id")
-          if not company_id then
-            local body, err, mimetype = kong.request.get_body()
-            if body then
-              if mimetype == "application/json" then
-                company_id = body.company_id
-              else
-                if mimetype == "multipart/form-data" then
-                  company_id = body.company_id
-                end
-              end
-            end
-          end
-          return company_id
-        end
+function ExternalAuthHandler:access(conf)
+  ExternalAuthHandler.super.access(self)
 
-        function ExternalAuthHandler:access(conf)
-          ExternalAuthHandler.super.access(self)
+  local client = http.new()
+  client:set_timeouts(conf.connect_timeout, send_timeout, read_timeout)
 
-          local client = http.new()
-          client:set_timeouts(conf.connect_timeout, send_timeout, read_timeout)
+  local res, err = client:request_uri(conf.url, {
+    path = conf.path,
+    query = {
+      auth_token = kong.request.get_header(conf.token_header),
+      company_id = get_company_id()
+    },
+    headers = {
+      Accepts = "application/json",
+      referer = kong.request.get_header("referer")
+    },
+    method = "GET"
+  })
 
-          local res, err = client:request_uri(conf.url, {
-            path = conf.path,
-            query = {
-              auth_token = kong.request.get_header(conf.token_header),
-              company_id = get_company_id()
-            },
-            headers = {
-              Accepts = "application/json",
-              referer = kong.request.get_header("referer")
-            },
-            method = "GET"
-          })
+  if not res then
+    return kong.response.exit(500, {message=err})
+  end
 
-          if not res then
-            return kong.response.exit(500, {message=err})
-          end
+  if res.status == 403 then
+    return kong.response.exit(403, {message=conf.message_403})
+  elseif res.status == 404 then
+    return kong.response.exit(404, {message=conf.message_404})
+  elseif res.status ~= 200 then
+    return kong.response.exit(401, {message=conf.message_401})
+  else
+    if not res.body then
+      return kong.response.exit(502, {message="no authentication response"})
+    end
 
-          if res.status == 403 then
-            return kong.response.exit(403, {message=conf.message_403})
-          elseif res.status == 404 then
-            return kong.response.exit(404, {message=conf.message_404})
-          elseif res.status ~= 200 then
-            return kong.response.exit(401, {message=conf.message_401})
-          else
-            if not res.body then
-              return kong.response.exit(502, {message="no authentication response"})
-            end
+    local decoded_body = json.decode(res.body)
+    if not decoded_body then
+      return kong.response.exit(502, {message="empty authentication response object"})
+    end
+    
+    local user = decoded_body["output"]
+    if not user then
+      return kong.response.exit(502, {message="no user details in authentication response"})
+    end
 
-            local decoded_body = json.decode(res.body)
-            if not decoded_body then
-              return kong.response.exit(502, {message="empty authentication response object"})
-            end
-            
-            local user = decoded_body["output"]
-            if not user then
-              return kong.response.exit(502, {message="no user details in authentication response"})
-            end
+    kong.service.request.set_header(conf.injection_header, json.encode(user))
+    if kong.request.get_header(conf.masking_header) then 
+      kong.service.request.set_header(conf.masking_header, kong.request.get_header(conf.masking_header))
+    end
+    
+    if decoded_body["company_details"] then
+        kong.service.request.set_header(conf.company_injection_header, json.encode(decoded_body["company_details"]))
+    end
+  end
+end
 
-            kong.service.request.set_header(conf.injection_header, json.encode(user))
-            if kong.request.get_header(conf.masking_header) then 
-              kong.service.request.set_header(conf.masking_header, kong.request.get_header(conf.masking_header))
-            end
-            
-            if decoded_body["company_details"] then
-                kong.service.request.set_header(conf.company_injection_header, json.encode(decoded_body["company_details"]))
-            end
-          end
-        end
+ExternalAuthHandler.PRIORITY = 900
+ExternalAuthHandler.VERSION = "0.2.1"
 
-        ExternalAuthHandler.PRIORITY = 900
-        ExternalAuthHandler.VERSION = "0.2.1"
-
-        return ExternalAuthHandler
+return ExternalAuthHandler
